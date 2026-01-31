57255671eeec83cd9c2562a8fc5ac9617de8bc83
Insha Sadaf <inshs123@gmail.com>
2026-01-31 20:40:09 +0530

    Refactor data processing in make_dataset.py and export processed dataset to pickle

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,38 +4,161 @@ from glob import glob
 # --------------------------------------------------------------
 # Read single CSV file
 # --------------------------------------------------------------
+single_file_acc = pd.read_csv(
+    '../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv'
+)
 
-single_file_acc = pd.read_csv('../../data/raw/MetaMotion/data\raw\MetaMotion\E-row-medium_MetaWear_2019-01-18T18.33.07.895_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv')
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-rest-standing_MetaWear_2019-01-18T18.25.39.382_C42732BE255C_Gyroscope_25.000Hz_1.4.41.csv"
+)
 
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
-
-
+files= glob("../../data/raw/MetaMotion/*.csv")
+len(files)
 # --------------------------------------------------------------
 # Extract features from filename
 # --------------------------------------------------------------
+data_path ="../../data/raw/MetaMotion\\"
+f = files[0]
 
+participant = f.split("-")[0].replace(data_path,"")
+label = f.split("-")[1]
+category= f.split("-")[2].rstrip("123")
+
+df = pd.read_csv(f)
+df['participant'] = participant
+df['label'] = label
+df['category'] = category
 
 # --------------------------------------------------------------
 # Read all files
 # --------------------------------------------------------------
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
 
 
+for f in files:
+    participant = f.split("-")[0].replace(data_path,"")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+    df=pd.read_csv(f)
+    df['participant'] = participant
+    df['label'] = label
+    df['category'] = category
+
+    if "Accelerometer" in f:
+        df['set']=acc_set
+        acc_set+=1
+        acc_df = pd.concat([acc_df,df])
+    elif "Gyroscope" in f:
+        df['gyr']=gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df,df])
+
+acc_df[acc_df['set']==10].head()
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.info()
+
+pd.to_datetime(df['epoch (ms)'],unit='ms')
+pd.to_datetime(df['time (01:00)'])
 
 
+
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"],unit ="ms")
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"],unit = "ms")
+
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+
+del gyr_df["epoch (ms)"]
+del gyr_df["time (01:00)"]
+del gyr_df["elapsed (s)"]
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
 
+files = glob("../../data/raw/MetaMotion/*.csv")
 
+def read_data_from_files(files):
+
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+
+    acc_set = 1
+    gyr_set = 1
+
+    for f in files:
+        participant = f.split("-")[0].replace(data_path,"")
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+        df=pd.read_csv(f)
+        df['participant'] = participant
+        df['label'] = label
+        df['category'] = category
+
+        if "Accelerometer" in f:
+            df['set']=acc_set
+            acc_set+=1
+            acc_df = pd.concat([acc_df,df])
+        elif "Gyroscope" in f:
+            df['gyr']=gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df,df])
+
+    # acc_df[acc_df['set']==10].head()
+
+    # pd.to_datetime(df['epoch (ms)'],unit='ms')
+    # pd.to_datetime(df['time (01:00)'])
+
+
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"],unit ="ms")
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"],unit = "ms")
+
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+
+
+    del gyr_df["epoch (ms)"]
+    del gyr_df["time (01:00)"]
+    del gyr_df["elapsed (s)"]
+
+    return acc_df,gyr_df
+
+acc_df,gyr_df = read_data_from_files(files)
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
 
+data_merged = pd.concat([acc_df.iloc[:,:3],gyr_df],axis=1) # column wise
+#data_merged.dropna()
+
+# rename columns 
+data_merged.columns=[
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "label",
+    "category",
+    "participant",
+    "set",
+]
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
@@ -44,7 +167,28 @@ single_file_acc = pd.read_csv('../../data/raw/MetaMotion/data\raw\MetaMotion\E-r
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
+sample ={
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyr_x": "mean",
+    "gyr_y": "mean",
+    "gyr_z": "mean",
+    "label":"last",
+    "category":"last",
+    "participant":"last",
+    "set":"last",
+}
+data_merged[:1000].resample(rule = "200ms").apply(sample)
 
+#split by day
+days =[g for n,g in data_merged.groupby(pd.Grouper(freq="D"))]
+data_resampled = pd.concat([df.resample(rule = "200ms").apply(sample).dropna() for df in days])
+data_resampled.info()
+
+data_resampled["set"]= data_resampled["set"].astype("int")
 # --------------------------------------------------------------
 # Export dataset
 # -------------------------------------------------------------- 
+# explorting the dataframe in searlized format
+data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
